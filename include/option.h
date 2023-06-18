3f2f0072dc640ec4f1b47711c0e10a48f12acf97
lmpizarro <lmpizarro@gmail.com>
2023-06-18 19:59:46 -0300

    ..

--- include/option.h
+++ include/option.h
@@ -10,10 +10,10 @@ class Option
 {
 public:
     Option();
-    Option(const float& _S0, const float& _K, const float& _T,
-        const float& _r, const float& _sigma, const float& _Q);
-    Option(const Option & _option);
-    Option& operator=(const Option& rhs);
+    Option(const float &_S0, const float &_K, const float &_T,
+           const float &_r, const float &_sigma, const float &_Q);
+    Option(const Option &_option);
+    Option &operator=(const Option &rhs);
     float gS0() const { return S0; };
     float gK() const { return K; };
     float gexT() const { return T; }
@@ -26,7 +26,7 @@ public:
     virtual float deltaC() const = 0;
     virtual float deltaP() const = 0;
     virtual float thetaC(bool) const = 0;
-    virtual float thetaP(bool)  const = 0;
+    virtual float thetaP(bool) const = 0;
     virtual float rhoC() const = 0;
     virtual float rhoP() const = 0;
     virtual float vega() const = 0;
@@ -35,10 +35,12 @@ public:
     friend std::ostream &operator<<(std::ostream &os, const Option &s);
 
     virtual void print(std::ostream &o) const = 0;
+
 protected:
     float S0, K, T, r, sigma, Q;
+
 private:
-    void copy(const Option&);
+    void copy(const Option &);
 };
 
 inline std::ostream &operator<<(std::ostream &os, const Option &option)
@@ -46,8 +48,6 @@ inline std::ostream &operator<<(std::ostream &os, const Option &option)
     option.print(os);
     return os;
 }
-
-
 
 class BSM : public Option
 {
@@ -59,18 +59,18 @@ private:
     float grealT() const;
 
 public:
-    BSM(const float& S0, const float& K, const float& T,
-        const float& r, const float& sigma, const float& Q);
+    BSM(const float &S0, const float &K, const float &T,
+        const float &r, const float &sigma, const float &Q);
     float d1() const;
     float d2() const;
     float C() const;
     float P() const;
     float deltaC() const;
-    float deltaP() const ;
+    float deltaP() const;
     float thetaC(bool) const;
     float thetaP(bool) const;
     float rhoC() const;
-    float rhoP() const ;
+    float rhoP() const;
     float vega() const;
     float gamma() const;
     void setIV(float);
